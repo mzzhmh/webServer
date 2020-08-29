a35b1b0b51441840b2fc265245352eceb5170ed6
root <root@localhost.localdomain>
2020-08-29 10:39:08 -0400

    nsw refresh get offset records

--- nswDetailed/refreshNSWdetails.py
+++ nswDetailed/refreshNSWdetails.py
@@ -28,6 +28,8 @@ with open(conffile) as f:
     webhost = lines[4].strip().split('=')[1]
     webport = lines[5].strip().split('=')[1]
 
+
+print("\n===============================================================")
 #get local db num
 try:
     cnx = mysql.connector.connect(user=dbuser, password=dbpw,host=dbhost,database=dbase)
@@ -45,7 +47,33 @@ totURL='https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=2
 JSONtotNum = requests.get(totURL,verify=False,timeout=30)
 if (JSONtotNum.status_code == 200):
     jsonData = json.loads(JSONtotNum.text)
-    print("[RES]:"+str(jsonData["result"]["total"]))
+    webNum = jsonData["result"]["total"]
+    print("[RES]:"+str(webNum))
 else:
     print("[ERR]:"+JSONtotNum.text)
 
+#how many to get
+offset = webNum - dbNum
+if offset <= 0:
+    print("DB ahead, wait.....")
+    sys.exit(1)
+else:
+    newCasesURL='https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29&offset=3780&limit='+str(offset)
+    print("[OFFSET]:"+str(offset))
+    JSONoffset = requests.get(newCasesURL,verify=False,timeout=30)
+    if (JSONoffset.status_code == 200):
+        OffsetJSONdata = json.loads(JSONoffset.text)
+        records = OffsetJSONdata['result']['records']
+        #print(json.dumps(records, indent=4, sort_keys=True))
+        
+        #put each record into local db
+        for entry in records:
+            print(entry)
+    else:
+        print("[ERR]:"+JSONoffset.text)
+
+
+
+
+
+print("==================================================================")
